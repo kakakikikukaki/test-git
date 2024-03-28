e42bc0721873d1ebd4c4127fcc451392b576ec6a
iAmKi <rizkitanjung001@outlook.com>
2024-03-28 16:24:37 +0700

    add test case & test suites login FB

---
+++ Scripts/open fb/Script1711617560520.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F')
+
+WebUI.setText(findTestObject('Object Repository/login fb/Page_Log in to Facebook/input_You must log in to continue_email'), 
+    'kiki.tanjung1999@gmail.com')
+
+WebUI.setEncryptedText(findTestObject('Page_Log in to Facebook/input_You must log in to continue_pass'), 'CBg7Uv88h2yK3yLsfBuW1A==')
+
+WebUI.click(findTestObject('Object Repository/login fb/Page_Log in to Facebook/button_Log in'))
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/login fb.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>login fb</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2078e83e-8a68-47c7-b921-1c9c2b45553f</testSuiteGuid>
+   <testCaseLink>
+      <guid>30e0db9d-88a1-4ba3-bf50-5f210ecd212e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/open fb</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
