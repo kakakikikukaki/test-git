9048bbe604d1aec773a3d915fed04cecb2a231b7
iAmKi <rizkitanjung001@outlook.com>
2024-04-09 15:52:57 +0700

    asc

---
+++ Object Repository/selector hub/Page_Xpath Practice Page  Shadow dom, neste_c4f084/input_Mobile Number_mobile number.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Mobile Number_mobile number</name>
+   <tag></tag>
+   <elementGuidId>51fa0353-2872-404a-9b70-0d0bf32061a5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>input[name=&quot;mobile number&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name='mobile number']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=spinbutton</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+      <webElementGuid>1f9d0d40-fa46-43f1-9557-66d4f4cd5a36</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>mobile number</value>
+      <webElementGuid>40729b74-86d8-4f76-b596-c80614656a66</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>number</value>
+      <webElementGuid>b8d29bf0-1d25-4262-8be2-41ebc2239fab</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-control</value>
+      <webElementGuid>92688142-c44c-405f-8b92-797aff4e94ad</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>placeholder</name>
+      <type>Main</type>
+      <value>Enter your mobile number</value>
+      <webElementGuid>59437e72-36ba-43cc-99d0-c37941f9ecb5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;content&quot;)/div[@class=&quot;page-content&quot;]/div[@class=&quot;elementor elementor-1097&quot;]/section[@class=&quot;elementor-section elementor-top-section elementor-element elementor-element-0731668 elementor-section-boxed elementor-section-height-default elementor-section-height-default&quot;]/div[@class=&quot;elementor-container elementor-column-gap-default&quot;]/div[@class=&quot;elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-b7d792b&quot;]/div[@class=&quot;elementor-widget-wrap elementor-element-populated&quot;]/div[@class=&quot;elementor-element elementor-element-459c920 elementor-widget__width-inherit elementor-widget elementor-widget-html&quot;]/div[@class=&quot;elementor-widget-container&quot;]/div[@class=&quot;userform&quot;]/div[@class=&quot;content block panel check-height  parent-inline-cont&quot;]/div[@class=&quot;element-companyId&quot;]/div[@class=&quot;form-group&quot;]/div[@class=&quot;input-group input-block&quot;]/input[@class=&quot;form-control&quot;]</value>
+      <webElementGuid>a7bc6744-3885-4448-9239-d8951126ef71</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//input[@name='mobile number']</value>
+      <webElementGuid>dd820db7-34d2-4a18-a501-98921424322f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//main[@id='content']/div/div/section[2]/div/div/div/div/div/div/div/div/div/div/input[2]</value>
+      <webElementGuid>f7e160b7-ce2f-4450-b2b0-041b04903f65</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div/div/div/div/div/input[2]</value>
+      <webElementGuid>643275f3-cb2c-4f06-a041-818bfec4e02c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//input[@name = 'mobile number' and @type = 'number' and @placeholder = 'Enter your mobile number']</value>
+      <webElementGuid>01aed0eb-1bdc-40fb-8263-b7079dac4ac2</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
