64f0ba5d8d5fb5fd610a8978c544273b0bc8f864
iAmKi <rizkitanjung001@outlook.com>
2024-04-09 16:03:52 +0700

    dk

---
+++ Object Repository/webdriver/Page_WebDriverUniversity.com/h1_POPUP  ALERTS.rs
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h1_POPUP  ALERTS</name>
+   <tag></tag>
+   <elementGuidId>d0c4decd-7f19-407b-92e5-1148de10cefa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#popup-alerts > div.thumbnail > div.section-title > h1</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='popup-alerts']/div/div/h1</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;POPUP &amp; ALERTS Close those annoying Popup &amp; Alerts! At times Popup and alerts can be quite annoying especially when they can impact the results of your automation test scripts. There are various types of popups and alerts which are present such as ‘JavaScript’ alerts or even ‘Modal’ popups. Once you understand the way to handle such popups and alerts the process can be straight forward! provided you keep practising 😊&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+      <webElementGuid>64db0510-1aa4-4302-a011-c5a373e3c84a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>POPUP &amp; ALERTS</value>
+      <webElementGuid>c5ddd76d-3c48-4a11-b9f1-75e8e46a8934</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;popup-alerts&quot;)/div[@class=&quot;thumbnail&quot;]/div[@class=&quot;section-title&quot;]/h1[1]</value>
+      <webElementGuid>3d32e2b0-aa33-48b4-8a51-a2ba24c30e07</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//a[@id='popup-alerts']/div/div/h1</value>
+      <webElementGuid>54b47067-3363-4635-8398-8c0ec002ceee</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Scrolling Around...'])[1]/following::h1[1]</value>
+      <webElementGuid>7e23d2c5-1526-4d39-8e8d-4ebd6ca05b2f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='SCROLLING AROUND'])[1]/following::h1[1]</value>
+      <webElementGuid>d83c9dcb-b65c-4863-8111-e4424dc3588a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Close those annoying Popup &amp; Alerts!'])[1]/preceding::h1[1]</value>
+      <webElementGuid>c9efc8e5-86a2-4a46-8ec8-0baacd9916e9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='IFRAME'])[1]/preceding::h1[1]</value>
+      <webElementGuid>80a5f8b6-5475-437d-b287-16964e0d7740</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='POPUP &amp; ALERTS']/parent::*</value>
+      <webElementGuid>335ececa-928e-4fe3-9803-afef4c89cfcf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[11]/a/div/div/h1</value>
+      <webElementGuid>84e26737-a634-478c-86f3-165801f36228</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h1[(text() = 'POPUP &amp; ALERTS' or . = 'POPUP &amp; ALERTS')]</value>
+      <webElementGuid>3a38c158-3f9c-4e0a-85c2-2a29c8b7d6f8</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
